32ed9d4e9bfa95fa814099f8471872329559e6d9
rukspui <ruxandrapui@yahoo.com>
2016-01-26 13:52:20 +0200

    updated forms

--- views/site/events.php
+++ views/site/events.php
@@ -24,12 +24,17 @@ $this->params['breadcrumbs'][] = $this->title;
       <input name="group1" type="radio" id="female" />
       <label for="female">female</label>
     </p>
-    
-    <div id="date-picker">
-    <label for="birthdate" class="">Birthdate</label>
-      <input id="birthdate" type="text" class="datepicker picker__input" readonly="" tabindex="-1" aria-haspopup="true" aria-expanded="false" aria-readonly="false" aria-owns="birthdate_root">
-      
-    </div>
+
+     <div class="input-field col s12 m6">
+         <p>My age</p>
+    <select>
+      <option value="" disabled selected>is between</option>
+      <option value="1">16-21 yo</option>
+      <option value="2">22-35 yo</option>
+      <option value="3">35+ yo</option>
+    </select>
+   
+  </div>
 
     
     <p>who is a</p>
@@ -58,6 +63,42 @@ $this->params['breadcrumbs'][] = $this->title;
       <label for="volunteer">volunteer</label>
     </p>
 
+    
+      <p>and I am interested in</p>
+      <p>
+      <input type="checkbox" id="science" />
+      <label for="science">science</label>
+    </p>
+    <p>
+      <input type="checkbox" id="music"/>
+      <label for="music">music</label>
+    </p>
+     <p>
+      <input type="checkbox" id="dance"/>
+      <label for="dance">dance</label>
+    </p>
+     <p>
+      <input type="checkbox" id="IT"/>
+      <label for="IT">IT</label>
+    </p>
+
+    
+    
+    
+          <p>I prefer resources that are</p>
+      <p>
+      <input type="checkbox" id="video" />
+      <label for="video">video</label>
+    </p>
+       <p>
+      <input type="checkbox" id="audio" />
+      <label for="audio">audio</label>
+    </p>
+       <p>
+      <input type="checkbox" id="text" />
+      <label for="text">text</label>
+    </p>
+<button type="submit" class="btn btn-primary" name="login-button">Surprise me</button>
   </form>
    
     
