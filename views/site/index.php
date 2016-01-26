907232dfb2fa1750414739161a1bd2ced04cb6c6
rukspui <ruxandrapui@yahoo.com>
2016-01-26 12:27:32 +0200

    updated homepage

--- views/site/index.php
+++ views/site/index.php
@@ -13,13 +13,14 @@ $this->title = 'Go EU';
 
                 <div class="container">
                 
-                <h1>Everyone is an entrepreneur.</h1>
+                <h1>Welcome</h1>
 
-                <p class="lead">"The only skills you need to be an entrepreneur: an ability to fail, an ability to have ideas, to sell those ideas, to execute on those ideas, and to be persistent so even as you fail you learn and move onto the next adventure." — James Altucher</p>
+                <p class="lead">Start IT up is a platform that will help you develop the skills you need, find the resources you were looking for and even the ones you didn't know you were looking for!</p>
+                    <p>Stop procrastinating, just START IT UP!</p>
 
                 <div class="row center">
                 <a href="index.php?r=site%2Flogin" id="download-button" class="btn-large waves-effect waves-light orange">Get started</a>
-              <a href="#" id="download-button" class="btn-large waves-effect waves-light orange">Learn more</a>
+              <a href="index.php?r=site%2Fabout" id="download-button" class="btn-large waves-effect waves-light orange">Learn more</a>
               </div>
                 
                        <div class="jumbotron-images">
@@ -37,13 +38,14 @@ $this->title = 'Go EU';
                        <div class="jumbotron-images">
                     <img src=></a>
                 </div>
-                <h1>Congratulations! 2</h1>
+                <h1>Start today</h1>
 
-                <p class="lead">The only skills you need to be an entrepreneur: an ability to fail, an ability to have ideas, to sell those ideas, to execute on those ideas, and to be persistent so even as you fail you learn and move onto the next adventure." — James Altucher</p>
+                <p class="lead">Here you can find resources, and more important, motivation from people that are on the same path as you: developing and improving themselves! </p>
+                <p>Never stop learning!</p>
 
                 <div class="row center">
-                <a href="index.php?r=site%2Flogin" id="download-button" class="btn-large waves-effect waves-light orange">Get started</a>
-              <a href="#" id="download-button" class="btn-large waves-effect waves-light orange">Learn more</a>
+                <a href="index.php?r=site%2Fresources" id="download-button" class="btn-large waves-effect waves-light orange">Resources</a>
+              <a href="index.php?r=site%2Fevents" id="download-button" class="btn-large waves-effect waves-light orange">Get inspired</a>
               </div>
                 </div>
             </div>
@@ -54,13 +56,13 @@ $this->title = 'Go EU';
                        <div class="jumbotron-images">
                     <img src=></a>
                 </div>
-                <h1>Congratulations! 3</h1>
+                <h1>Ready to learn something new?</h1>
 
-                <p class="lead">The only skills you need to be an entrepreneur: an ability to fail, an ability to have ideas, to sell those ideas, to execute on those ideas, and to be persistent so even as you fail you learn and move onto the next adventure." — James Altucher</p>
+                <p class="lead">Browse the Resources section and find all sorts of materials, from video to audio and articles. You don't know what you are looking for? We do! Just go to the Get inspired page and we will find something suitable for you!</p>
 
                 <div class="row center">
-                <a href="index.php?r=site%2Flogin" id="download-button" class="btn-large waves-effect waves-light orange">Get started</a>
-              <a href="#" id="download-button" class="btn-large waves-effect waves-light orange">Learn more</a>
+                <a href="index.php?r=site%2Fresources" id="download-button" class="btn-large waves-effect waves-light orange">Resources</a>
+              <a href="index.php?r=site%2Fevents" id="download-button" class="btn-large waves-effect waves-light orange">Get inspired</a>
               </div>
                 </div>
             </div>
@@ -72,34 +74,25 @@ $this->title = 'Go EU';
 
         <div class="row">
             <div class="col-lg-12">
-                <h2>Heading</h2>
+                <h2>TED Talks</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>TED is a global community, welcoming people from every discipline and culture who seek a deeper understanding of the world. We believe passionately in the power of ideas to change attitudes, lives and, ultimately, the world. On TED.com, we're building a clearinghouse of free knowledge from the world's most inspired thinkers — and a community of curious souls to engage with ideas and each other, both online and at TED and TEDx events around the world, all year long.</p>
 
-                <p><a class="btn btn-primary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-primary" href="https://www.ted.com/">TED website &raquo;</a></p>
             </div>
             <div class="col-lg-12">
-                <h2>Heading</h2>
+                <h2>Medium</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>edium is a different kind of place to read and write on the internet. A place where the measure of success isn’t views, but viewpoints. Where the quality of the idea matters, not the author’s qualifications. A place where conversation pushes ideas forward and words still matter.</p>
 
-                <p><a class="btn btn-primary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <p><a class="btn btn-primary" href="https://medium.com/">Medium website &raquo;</a></p>
             </div>
             <div class="col-lg-12">
-                <h2>Heading</h2>
+                <h2>Coursera</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>Coursera is an education platform that partners with top universities and organizations worldwide, to offer courses online for anyone to take..</p>
 
-                <p><a class="btn btn-primary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-primary" href="https://www.coursera.org/">Coursera website &raquo;</a></p>
             </div>
         </div>
         
